7b30b14b770e15b2a4372e661987c949ad2160de
reza.solghar@gmail.com <mras.arab@gmail.com>
2024-07-31 09:54:45 +0200

    customer service finished and error handlers added

---
+++ services/customer/src/main/java/com/pooya/ecommerce/customer/CustomerRequest.java
@@ -0,0 +1,17 @@
+package com.pooya.ecommerce.customer;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public record CustomerRequest(
+        String id,
+        @NotNull(message = "Customer firstname is required")
+        String firstname,
+        @NotNull(message = "Customer lastname is required")
+        String lastname,
+        @Email(message = "Customer email is not valid")
+        String email,
+        Address address
+) {
+
+}
---
+++ services/customer/src/main/java/com/pooya/ecommerce/customer/CustomerResponse.java
@@ -0,0 +1,11 @@
+package com.pooya.ecommerce.customer;
+
+public record CustomerResponse(
+        String id,
+        String firstname,
+        String lastname,
+        String email,
+        Address address
+) {
+
+}
